3118625aeff548c828ffe4e362e2c926a15f1349
Bert Tejeda <etejeda@tecknicos.com>
2022-04-23 17:11:42 -0400

    Installer should be a function

--- .installer.sh
+++ .installer.sh
@@ -1,3 +1,5 @@
+function bert.bash.install() {
+
  argscount=$#
   allargs=${@}
 
@@ -8,7 +10,7 @@
     --help
   """
 
-BERT_BASH_GIT_URL="https://github.com/berttejeda/bert.bash.git"
+  BERT_BASH_GIT_URL="https://github.com/berttejeda/bert.bash.git"
 
   while (( "$#" )); do
       if [[ "$1" =~ --bert-bash-git-url|^-g$ ]]; then BERT_BASH_GIT_URL=$2;fi    
@@ -23,37 +25,42 @@ BERT_BASH_GIT_URL="https://github.com/berttejeda/bert.bash.git"
     return
   fi
 
-BERT_BASH_HOME="${HOME}/.bert.bash"
+  BERT_BASH_HOME="${HOME}/.bert.bash"
 
-echo -n "Checking if bert.bash is installed ... "
+  echo -n "Checking if bert.bash is installed ... "
 
-if [[ ! -d "${HOME}/.bert.bash" ]];then
-    echo 'Installing bert.bash'
-    git clone $BERT_BASH_GIT_URL "${BERT_BASH_HOME}"
-    echo "source ${BERT_BASH_HOME}/.installer.sh" >> "${HOME}/.bash_profile"
-elif [[ (-d "${BERT_BASH_HOME}") && ($BERT_BASH_UPDATE) ]];then
-    echo -n "updating bert.bash ... "
-    cd "${BERT_BASH_HOME}"
-    git clean -f
-    git checkout .
-    git pull
-    echo "done"
-else
-    echo 'no need to install or update'
-fi
+  if [[ ! -d "${HOME}/.bert.bash" ]];then
+      echo 'Installing bert.bash'
+      git clone $BERT_BASH_GIT_URL "${BERT_BASH_HOME}"
+      echo "source ${BERT_BASH_HOME}/.installer.sh" >> "${HOME}/.bash_profile"
+  elif [[ (-d "${BERT_BASH_HOME}") && ($BERT_BASH_UPDATE) ]];then
+      echo -n "updating bert.bash ... "
+      pushd "${PWD}"
+      cd "${BERT_BASH_HOME}"
+      git clean -f
+      git checkout .
+      git pull
+      echo "done"
+      popd
+  else
+      echo 'no need to install or update'
+  fi
 
-echo "Commencing imports!"
+  echo "Commencing imports!"
 
-for script in ${BERT_BASH_HOME}/*.sh; do
-start=`date +%s`
-if eval source $script;then
-    end=`date +%s`
-    runtime=$((end-start))      
-    echo -e "${green}${runtime}s: Imported ${script}${reset}"
-else
-    echo -e "${red}Failed to import ${script}${reset}"
-fi
-done  
+  for script in ${BERT_BASH_HOME}/*.sh; do
+  start=`date +%s`
+  if eval source $script;then
+      end=`date +%s`
+      runtime=$((end-start))      
+      echo -e "${green}${runtime}s: Imported ${script}${reset}"
+  else
+      echo -e "${red}Failed to import ${script}${reset}"
+  fi
+  done
+}
+
+bert.bash.install
 
 echo -e "${yellow}Wassup home skillet!${reset}"
 echo -e "You are logged in as ${bold}${USER-USERNAME}${reset}"
